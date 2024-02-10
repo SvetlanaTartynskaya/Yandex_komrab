7883bdfa3436d20117f6f013d01464aad8a04e28
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-02-10 15:53:05 +0300

    3

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import sys
-
 from PyQt5.QtGui import QPixmap
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
 import requests
 
 STATIC_API_SERVER = "https://static-maps.yandex.ru/1.x/"
@@ -13,14 +10,14 @@ STATIC_API_SERVER = "https://static-maps.yandex.ru/1.x/"
 def get_map_from_coords(coords, type_in=str):
     params = {
         "ll": coords if type_in is str else ",".join(coords),
-        "spn": "0.025,0.025",
+        "spn": "0.05,0.05",
         "l": "map"
     }
 
-    response = requests.request(method="GET", url=STATIC_API_SERVER,
-                                params=params)
-    if response.status_code == 200:
-        return response.content
+    res = requests.request(method="GET", url=STATIC_API_SERVER, params=params)
+
+    if res.status_code == 200:
+        return res.content
     return None
 
 
@@ -31,7 +28,8 @@ class MyWidget(QMainWindow):
         self.search_btn.clicked.connect(self.search)
 
     def search(self):
-        image = get_map_from_coords(reversed(self.line_enter_coords.text().replace(" ", "").split(",")), type_in=list)
+        coords = (self.line_enter_coords.text().replace(" ", "").split(","))
+        image = get_map_from_coords(coords, type_in=list)
         pixmap = QPixmap()
         pixmap.loadFromData(image)
         self.map.setPixmap(pixmap)
